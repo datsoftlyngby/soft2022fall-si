e34c5fdecbfef679012533fd6d8a346ed656c942
Dora Di <tdi@cphbusiness.dk>
2022-11-22 10:28:02 +0100

    week 47

--- Code/P9-Microservices/src/main/java/dk/dd/discover/DiscoverApplication.java
+++ Code/P9-Microservices/src/main/java/dk/dd/discover/DiscoverApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class DiscoverApplication
 {
-
     public static void main(String[] args)
     {
         SpringApplication.run(DiscoverApplication.class, args);
